1c82161a3a808bbc36d3374080eeed3cc6a4d009
Rakesh Mohan Tarai <rakesh.mohan1999@gmail.com>
2023-07-08 13:56:05 +0530

    Mars-Rover-Coding-Ninjas

---
+++ index.js
@@ -0,0 +1,42 @@
+$(function() {
+  $("#date-picker").datepicker();
+
+  $("#submit-btn").click(function(e) {
+    e.preventDefault();
+
+    var selectedDate = $("#date-picker").val();
+
+    if (selectedDate === '') {
+      alert("Please select a date.");
+      return;
+    }
+
+    var apiUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY`;
+    displayImages(apiUrl);
+  });
+});
+
+function displayImages(apiUrl) {
+  // Clear existing images
+  $("#image-container").empty();
+
+  // Make a request to the NASA API
+  $.ajax({
+    url: apiUrl,
+    method: 'GET',
+    success: function(response) {
+      if (response.photos.length > 0) {
+        response.photos.forEach(function(photo) {
+          var imageSrc = photo.img_src;
+          var imageElement = $("<img>").attr("src", imageSrc);
+          $("#image-container").append(imageElement);
+        });
+      } else {
+        alert("No images found for the selected date.");
+      }
+    },
+    error: function() {
+      alert("An error occurred while fetching images.");
+    }
+  });
+}
